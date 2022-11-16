2fe32d68423f640725b0c12f70ed1a485825896d
Ola Grabowska <ola.grabowska.456@wp.pl>
2022-11-16 16:31:43 +0100

    funkcje prevPer i nextPer

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 //#include <bits/stdc++.h> zawiera funkcje z różnych nagłówków, więc nie trzeba importować innych np.<math.h> do obliczania sqrt()
 using namespace std;
 
-
-void permutacja(string s, int l, int p) 
+//wyświetla wszystkie permutacje
+void permutacje(string s, int l, int p) 
 { 
     if (l == p) 
         cout<<s<<endl; 
@@ -11,12 +11,36 @@ void permutacja(string s, int l, int p)
     { 
         for (int i = l; i <= p; i++) 
         { 
+// swap() --> funkcja pobrana z nagłówka <bits/stdc++.h> 
             swap(s[l], s[i]); 
-            permutacja(s, l+1, p); 
+            permutacje(s, l+1, p); 
             swap(s[l], s[i]); 
         } 
     } 
 } 
+
+// następna permutacja w kolejności leksykograficznej
+void nextPer(string str)
+{
+	bool x = next_permutation(str.begin(), str.end());
+    if (x == false)
+        cout << "Brak następnej permutacji w kolejności leksykograficznej"
+            << endl;
+    else
+        cout << str << endl;
+}
+
+// poprzednia permutacja w kolejności leksykograficznej
+void prevPer(string str)
+{
+	bool x = prev_permutation(str.begin(), str.end());
+    if (x == false)
+        cout << "Brak poprzedniej permutacji w kolejności leksykograficznej"
+            << endl;
+    else
+        cout << str << endl;
+}
+
 
 int main()
 {
@@ -24,7 +48,10 @@ int main()
 	cout<<"Podaj ciąg znaków: ";
 	cin>>str;
     int n = str.size(); 
-    permutacja(str, 0, n-1); 
+    permutacje(str, 0, n-1); 
+	nextPer(str);
+	prevPer(str);
     return 0; 
+// system() włącza skrypt bash myfile.sh
 	system("/home/user/permutacje_pr/myfile.sh");
 }
